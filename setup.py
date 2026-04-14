468c827f1897f49cea1f67d4ae88d86036fbd6a7
mandarwagh9 <mandarwagh90@gmail.com>
2026-04-13 23:08:52 -0700

    chore: update requirements and add ROS2 setup files
    - Add ROS2 dependencies to requirements.txt
    - Add setup.py for colcon build
    - Add resource folder for ROS2 package

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'openeyes'
+
+setup(
+    name=package_name,
+    version='2.5.0',
+    packages=['src'],
+    package_dir={'src': 'src'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Mandar Wagh',
+    maintainer_email='mandar@example.com',
+    description='Hardware-agnostic edge robot vision framework with world models',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'openeyes = src.main:main',
+        ],
+    },
+)
